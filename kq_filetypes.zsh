cc0ea970c9a5c6c01d19ee53e347bacaf6a7c52e
indieterminacy <indieterminacy@nortonspeal.com>
2017-10-07 02:07:54 +0200

    Renamed filename for configuration settings and performed some cleaning

--- kq_filetypes.zsh
+++ kq_filetypes.zsh
@@ -2,11 +2,11 @@
 
 bindkey -v
 ########################################
-alias -g PIPE='|'
+# alias -g PIPE='|'
 # alias -g CA="2>&1 | cat -A"
 # alias -g DN=/dev/null
 # alias -g F=' | fmt -'
-alias -g Sk="*~(*.bz2|*.gz|*.tgz|*.zip|*.z)"
+alias -g KQ="*~(*.bz2|*.gz|*.tgz|*.zip|*.z)"
 # alias -g MM='| most'
 # alias -g NE="2> /dev/null"
 # alias -g NUL="> /dev/null 2>&1"
